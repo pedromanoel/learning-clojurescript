59e56bfe802996688a93fca785a99c5a08011831
Pedro Evangelista <pedro.evangelista@gmail.com>
2022-03-02 11:41:37 -0300

    Upgrade react

--- src/main/hello_world/core.cljs
+++ src/main/hello_world/core.cljs
@@ -1,6 +1,12 @@
 (ns hello-world.core
   (:require react-dom))
 
-(.render js/ReactDOM
-  (.createElement js/React "h2" nil "Hello, React!")
-  (.getElementById js/document "app"))
+(defonce root (->> (.getElementById js/document "app")
+                   (.createRoot js/ReactDOM)))
+
+(.render root
+         (.createElement js/React "h2" nil "Hello, React 1!"))
+
+(comment
+  (js/alert "Oi")
+  (println "oi"))
